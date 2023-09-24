6e8d03b940b3384771dfc13276817fe60fdd003b
Aqib Javaid <aqibjavaid.ahmed@systemsltd.com>
2023-09-24 23:22:19 +0500

    remove some warnings in multiple components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,43 @@ import RestaurantCard from "./components/RestaurantCard";
 import { Cuisine, Location, PRICING, PrismaClient } from "@prisma/client";
 
 export interface RestaurantCardType {
-	id: number;
-	name: string;
-	image: string;
-	slug: string;
-	cuisine: Cuisine;
-	location: Location;
-	pricing: PRICING;
+  id: number;
+  name: string;
+  image: string;
+  slug: string;
+  cuisine: Cuisine;
+  location: Location;
+  pricing: PRICING;
 }
 
 const prisma = new PrismaClient();
 
 const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
-	const restaurants = await prisma.restaurant.findMany(
-		{select: {
-			id: true,
-			name: true,
-			image: true,
-			slug: true,
-			pricing: true,
-			location: true,
-			cuisine: true,
-		}}
-	);
-	return restaurants;
-}
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      image: true,
+      slug: true,
+      pricing: true,
+      location: true,
+      cuisine: true,
+    },
+  });
+  return restaurants;
+};
 
 export default async function Home() {
-	const restaurants = await fetchRestaurants();
+  const restaurants = await fetchRestaurants();
 
-	return (
-		<main>
-			<Header />
-			<div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-				{restaurants.map((restaurant) => (
-					<RestaurantCard restaurant={restaurant} />
-				))}
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </main>
+  );
 }
